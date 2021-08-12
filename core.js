cf440bbc340a8e363c0ed9f8ebd668e5f7e1bb85
André Albuquerque <andre.albuquerque@fusiondms.com.br>
2021-08-12 12:09:55 -0300

    adicionando detalhes

--- core.js
+++ core.js
@@ -50,3 +50,20 @@ function closeSuperBox() {
 	if (closeSuperBox)
 		return closeSuperBox.parentNode.removeChild(closeSuperBox);
 }
+
+function mask(o, f) {
+	v_obj = o;
+	v_fun = f;
+	setTimeout(exeMask, 1);
+}
+
+function exeMask() {
+	v_obj.value = v_fun(v_obj.value);
+}
+
+function tel_br(v) {
+	v = v.replace(/\D/g, "");
+    v = v.replace(/^(\d\d)(\d)/g,"($1) $2");
+    v = v.replace(/(\d{5})(\d)/,"$1-$2");
+    return v;
+}
